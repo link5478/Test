8c47b8a4407f7a3370873cbdd3c4e94208a94b6a
link5478 <cheyne.carsten@gmail.com>
2017-11-17 01:04:19 +0000

    dsfdfsdfs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Larave
                 </div>
 
                 <div class="links">
